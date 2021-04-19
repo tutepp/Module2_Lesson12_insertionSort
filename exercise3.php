8a0767185521fe6cab8c2839dd54617495eb8680
tutepp <79822854+tutepp@users.noreply.github.com>
2021-04-19 16:01:14 +0700

    Add files via upload

---
+++ exercise3.php
@@ -0,0 +1,17 @@
+<?php
+
+function insertion_Sort($my_array)
+{
+    for ($i = 0; $i < count($my_array); $i++) {
+        $val = $my_array[$i];
+        $j = $i - 1;
+        while ($j >= 0 && $my_array[$j] > $val) {
+            $my_array[$j + 1] = $my_array[$j];
+            $j--;
+        }
+        $my_array[$j + 1] = $val;
+    }
+    return $my_array;
+}
+$array = [5, -4, 3, 7, 2, 1, 0, 8, 9, 2];
+var_dump( insertion_Sort($array));
